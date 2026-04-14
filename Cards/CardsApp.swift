6e810d6d3d93f58851edd734e45a95d2c2e8c462
fukuyori <self@spumoni.org>
2026-04-14 16:50:20 +0900

    Initial Commit

---
+++ Cards/CardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardsApp.swift
+//  Cards
+//
+//  Created by 福寄典明 on 2026/04/14.
+//
+
+import SwiftUI
+
+@main
+struct CardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
